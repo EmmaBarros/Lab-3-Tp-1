5ff226317db69907f6c700f1c84de07832dc19e6
Emma <emamito17baros@gmail.com>
2026-04-09 11:43:54 -0300

    comm

---
+++ src/Menu/Menu.java
@@ -0,0 +1,10 @@
+
+package Menu;
+
+/**
+ *
+ * @author emami
+ */
+public class Menu {
+    
+}
---
+++ src/Menu/Principal.java
@@ -0,0 +1,16 @@
+
+
+package Menu;
+
+/**
+ *
+ * @author emami
+ */
+public class Principal {
+
+  
+    public static void main(String[] args) {
+    
+    }
+    
+}
